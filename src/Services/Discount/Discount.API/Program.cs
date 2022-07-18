f4b699b96751360950cef3408073989d2c188ec7
szymbaramichal <michal.szymbara@rad4m.com>
2022-07-18 12:14:44 +0200

    discount

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,69 @@
+using Discount.API.Repositories;
+using Npgsql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var configuration = services.GetRequiredService<IConfiguration>();
+    var logger = services.GetRequiredService<ILogger<Program>>();
+
+    try
+    {
+        logger.LogInformation("Migrating postresql database.");
+
+        using var connection = new NpgsqlConnection
+            (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        connection.Open();
+
+        using var command = new NpgsqlCommand
+        {
+            Connection = connection
+        };
+
+        command.CommandText = "DROP TABLE IF EXISTS Coupon";
+        command.ExecuteNonQuery();
+
+        command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+        command.ExecuteNonQuery();
+
+        command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+        command.ExecuteNonQuery();
+
+        command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+        command.ExecuteNonQuery();
+
+        logger.LogInformation("Migrated postresql database.");
+    }
+    catch (NpgsqlException ex)
+    {
+        logger.LogError(ex, "An error occurred while migrating the postresql database");
+    }
+}
+
+app.Run();
